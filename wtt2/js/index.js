ccc0fa17ed2e470e43562faf54591720642992d3
gurkanietzsche <148501770+gurkanietzsche@users.noreply.github.com>
2023-11-17 01:44:47 +0300

    Add files via upload

---
+++ wtt2/js/index.js
@@ -0,0 +1,267 @@
+$(window).on('load',function(){
+  gsap.to('#loader',1,{y:"-100%"});
+  gsap.to('#loader',1,{opacity:0});
+  gsap.to('#loader',0,{display:"none",delay:1});
+  gsap.to('#header',0,{display:"block",delay:1})
+  gsap.to('#navigation-content',0,{display:"none"});
+  gsap.to('#navigation-content',0,{display:"flex",delay:1});
+})
+$(function(){
+  $('.color-panel').on("click",function(e) {
+    e.preventDefault();
+    $('.color-changer').toggleClass('color-changer-active');
+});
+$('.colors a').on("click",function(e) {
+  e.preventDefault();
+  var attr = $(this).attr("title");
+  console.log(attr);
+  $('head').append('<link rel="stylesheet" href="css/'+attr+'.css">');
+});
+});
+$(function(){
+     $('.menubar').on('click',function(){
+         gsap.to('#navigation-content',.6,{y:0});
+     })
+     $('.navigation-close').on('click',function(){
+        gsap.to('#navigation-content',.6,{y:"-100%"});
+    });
+   }); 
+
+$(function(){
+    var TxtRotate = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+      };
+      
+      TxtRotate.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+      
+        if (this.isDeleting) {
+          this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+          this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+      
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+      
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+      
+        if (this.isDeleting) { delta /= 2; }
+      
+        if (!this.isDeleting && this.txt === fullTxt) {
+          delta = this.period;
+          this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+          this.isDeleting = false;
+          this.loopNum++;
+          delta = 100;
+        }
+      
+        setTimeout(function() {
+          that.tick();
+        }, delta);
+      };
+      
+      window.onload = function() {
+        var elements = document.getElementsByClassName('txt-rotate');
+        for (var i=0; i<elements.length; i++) {
+          var toRotate = elements[i].getAttribute('data-rotate');
+          var period = elements[i].getAttribute('data-period');
+          if (toRotate) {
+            new TxtRotate(elements[i], JSON.parse(toRotate), period);
+          }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".txt-rotate > .wrap { border-right: 0em solid #666 ; }";
+        document.body.appendChild(css);
+      };
+})
+$(function(){
+
+    $('#about-link').on('click',function(){
+      gsap.to('#navigation-content',0,{display:"none",delay:.7});
+      gsap.to('#navigation-content',0,{y:'-100%',delay:.7});
+  gsap.to('#header',0,{display:"none"});
+gsap.to('#blog',0,{display:"none"});
+gsap.to('#portfolio',0,{display:"none"});
+   gsap.to('#breaker',0,{display:"block"});
+   gsap.to('#breaker-two',0,{display:"block",delay:.1});
+gsap.to('#contact',0,{display:"none"});
+   gsap.to('#breaker',0,{display:"none",delay:2});
+   gsap.to('#breaker-two',0,{display:"none",delay:2});
+   gsap.to('#about',0,{display:"block",delay:.7});
+   gsap.to('#navigation-content',0,{display:'flex',delay:2});
+ })
+ $('#contact-link').on('click',function(){
+   gsap.to('#navigation-content',0,{display:"none",delay:.7});
+   gsap.to('#navigation-content',0,{y:'-100%',delay:.7});
+gsap.to('#header',0,{display:"none"});
+gsap.to('#about',0,{display:"none"});
+gsap.to('#blog',0,{display:"none"});
+gsap.to('#portfolio',0,{display:"none"});
+gsap.to('#breaker',0,{display:"block"});
+gsap.to('#breaker-two',0,{display:"block",delay:.1});
+gsap.to('#breaker',0,{display:"none",delay:2});
+gsap.to('#breaker-two',0,{display:"none",delay:2});
+gsap.to('#contact',0,{display:"block",delay:.7});
+gsap.to('#navigation-content',0,{display:'flex',delay:2});
+})
+$('#portfolio-link').on('click',function(){
+  gsap.to('#navigation-content',0,{display:"none",delay:.7});
+  gsap.to('#navigation-content',0,{y:'-100%',delay:.7});
+gsap.to('#header',0,{display:"none"});
+gsap.to('#about',0,{display:"none"});
+gsap.to('#contact',0,{display:"none"});
+gsap.to('#blog',0,{display:"none"});
+gsap.to('#breaker',0,{display:"block"});
+gsap.to('#breaker-two',0,{display:"block",delay:.1});
+gsap.to('#breaker',0,{display:"none",delay:2});
+gsap.to('#breaker-two',0,{display:"none",delay:2});
+gsap.to('#portfolio',0,{display:"block",delay:.7});
+gsap.to('#navigation-content',0,{display:'flex',delay:2});
+})
+$('#blog-link').on('click',function(){
+  gsap.to('#navigation-content',0,{display:"none",delay:.7});
+  gsap.to('#navigation-content',0,{y:'-100%',delay:.7});
+gsap.to('#header',0,{display:"none"});
+gsap.to('#about',0,{display:"none"});
+gsap.to('#portfolio',0,{display:"none"});
+gsap.to('#contact',0,{display:"none"});
+gsap.to('#breaker',0,{display:"block"});
+gsap.to('#breaker-two',0,{display:"block",delay:.1});
+gsap.to('#breaker',0,{display:"none",delay:2});
+gsap.to('#breaker-two',0,{display:"none",delay:2});
+gsap.to('#blog',0,{display:"block",delay:.7});
+gsap.to('#navigation-content',0,{display:'flex',delay:2});
+})
+$('#home-link').on('click',function(){
+  gsap.to('#navigation-content',0,{display:"none",delay:.7});
+  gsap.to('#navigation-content',0,{y:'-100%',delay:.7});
+gsap.to('#header',0,{display:"none"});
+gsap.to('#about',0,{display:"none"});
+gsap.to('#portfolio',0,{display:"none"});
+gsap.to('#contact',0,{display:"none"});
+gsap.to('#blog',0,{display:"none"});
+gsap.to('#breaker',0,{display:"block"});
+gsap.to('#breaker-two',0,{display:"block",delay:.1});
+gsap.to('#breaker',0,{display:"none",delay:2});
+gsap.to('#breaker-two',0,{display:"none",delay:2});
+gsap.to('#header',0,{display:"block",delay:.7});
+gsap.to('#navigation-content',0,{display:'flex',delay:2});
+})
+})
+$(function(){
+ var body =  document.querySelector('body');
+ var $cursor = $('.cursor')
+   function cursormover(e){
+    
+    gsap.to( $cursor, {
+      x : e.clientX ,
+      y : e.clientY,
+      stagger:.002
+     })
+   }
+   function cursorhover(e){
+    gsap.to( $cursor,{
+     scale:1.4,
+     opacity:1
+    })
+    
+  }
+  function cursor(e){
+    gsap.to( $cursor, {
+     scale:1,
+     opacity:.6
+    }) 
+  }
+  $(window).on('mousemove',cursormover);
+  $('.menubar').hover(cursorhover,cursor);
+  $('a').hover(cursorhover,cursor);
+  $('.navigation-close').hover(cursorhover,cursor);
+
+})
+const angle = 20;
+const rotateCard = window;
+
+const lerp = (start, end, amount) => {
+	return (1 - amount) * start + amount * end;
+};
+
+const remap = (value, oldMax, newMax) => {
+	const newValue = ((value + oldMax) * (newMax * 2)) / (oldMax * 2) - newMax;
+	return Math.min(Math.max(newValue, -newMax), newMax);
+};
+
+window.addEventListener("DOMContentLoaded", (event) => {
+	const cards = document.querySelectorAll(".card");
+	cards.forEach((e) => {		
+		e.addEventListener("mousemove", (event) => {
+			const rect = e.getBoundingClientRect();
+			const centerX = (rect.left + rect.right) / 2;
+			const centerY = (rect.top + rect.bottom) / 2;
+			const posX = event.pageX - centerX;
+			const posY = event.pageY - centerY;
+			const x = remap(posX, rect.width / 2, angle);
+			const y = remap(posY, rect.height / 2, angle);
+			e.dataset.rotateX = x;
+			e.dataset.rotateY = -y;
+		});
+		
+		e.addEventListener("mouseout", (event) => {
+			e.dataset.rotateX = 0;
+			e.dataset.rotateY = 0;
+		});
+	});
+	
+	const update = () => {
+		cards.forEach((e) => {
+			let currentX = parseFloat(e.style.getPropertyValue('--rotateY').slice(0, -1));
+			let currentY = parseFloat(e.style.getPropertyValue('--rotateX').slice(0, -1));
+			if (isNaN(currentX)) currentX = 0;
+			if (isNaN(currentY)) currentY = 0;
+			const x = lerp(currentX, e.dataset.rotateX, 0.05);
+			const y = lerp(currentY, e.dataset.rotateY, 0.05);
+			e.style.setProperty("--rotateY", x + "deg");
+			e.style.setProperty("--rotateX", y + "deg");
+		})
+	}
+	setInterval (update,1000/60)
+});
+const button = document.querySelector("#button");
+
+const animate = (e) => {
+	const glow = document.createElement("div");
+	glow.classList.add("glow");
+	gsap.to(e.currentTarget, {
+		keyframes: [
+			{
+				duration: 2,
+				ease: "none",
+				onStart: () => {
+					button.appendChild(glow);
+					gsap.to(glow, {
+						inset: "-2em",
+						opacity: 0,
+						duration: 1.3
+					});
+				},
+				onComplete: () => {
+					button.removeChild(glow);
+				}
+			}
+		]
+	});
+};
+
+button.addEventListener("click", animate);
+
+
